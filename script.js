c1c10b5812ee2507781bfa4ba6e9ca5bca4dead1
Konstantin <rtrt28109@gmail.com>
2025-03-28 20:59:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,31 +8,49 @@
 // }
 
 
-function add() {
-    calculate((a, b) => a + b);
-}
+// function add() {
+//     calculate((a, b) => a + b);
+// }
 
-function subtract() {
-    calculate((a, b) => a - b);
-}
+// function subtract() {
+//     calculate((a, b) => a - b);
+// }
 
-function multiply() {
-    calculate((a, b) => a * b);
-}
+// function multiply() {
+//     calculate((a, b) => a * b);
+// }
 
-function divide() {
-    calculate((a, b) => {
-        if (b === 0) {
-            alert("Ошибка: Деление на ноль!");
-            return null;
-        }
-        return a / b;
-    });
-}
-function calculate(operation) {
-    const firstNumber = parseFloat(document.getElementById("firstNumber").value);
-    const secondNumber = parseFloat(document.getElementById("secondNumber").value);
+// function divide() {
+//     calculate((a, b) => {
+//         if (b === 0) {
+//             alert("Ошибка: Деление на ноль!");
+//             return null;
+//         }
+//         return a / b;
+//     });
+// }
+// function calculate(operation) {
+//     const firstNumber = parseFloat(document.getElementById("firstNumber").value);
+//     const secondNumber = parseFloat(document.getElementById("secondNumber").value);
     
-    const result = operation(firstNumber, secondNumber);
-    document.getElementById("result").innerText = "Результат: " + result;
-}
+//     const result = operation(firstNumber, secondNumber);
+//     document.getElementById("result").innerText = "Результат: " + result;
+// }
+
+// УРОК 10
+
+
+let city = document.querySelector(".city")
+
+fetch("https://api.openweathermap.org/data/2.5/weather?q=London&appid=d000fe065fbbc765b939d66078e4a40d&quot")
+.then(function(resp) {return resp.json()})
+.then(function(data){
+    console.log(data)
+    
+    city.innerHTML = data.sys.country
+    city.innerHTML = data.sys.sunrise
+    city.innerHTML = data.sys.sunset
+    city.innerHTML = data.wind.speed
+    city.innerHTML = data.name.humidity
+
+})
